f80c90174afdf035a2dced31689714473d61e00a
Ananth Rao <ananthamapod@gmail.com>
2016-10-12 17:25:05 -0400

    Add loaders for different types of files, start jade/pug templating, and add sass styling to test out linking of vendor styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 var webpack = require('webpack')
 var path = require('path')
+var htmlWebpackPlugin = require('html-webpack-plugin')
 
 var BUILD_DIR = path.resolve(__dirname, 'dist')
 var APP_DIR = path.resolve(__dirname, 'src')
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'index.js'
   },
   module: {
     loaders: [
@@ -16,9 +17,31 @@ var config = {
         test: /\.jsx?/,
         include: APP_DIR,
         loader: 'babel'
+      },
+      {
+        test: /\.scss?/,
+        include: APP_DIR,
+        loader: 'style!css!autoprefixer!sass'
+      },
+      {
+        test: /\.css?/,
+        include: '/',
+        loader: 'style!css!autoprefixer'
+      },
+      {
+        test: /\.pug|\.jade?/,
+        include: APP_DIR,
+        loader: 'pug-loader'
       }
     ]
-  }
+  },
+  plugins : [
+    new htmlWebpackPlugin({
+      template: APP_DIR + '/index.jade',
+      title: 'Auditor - Proto 2',
+      inject: 'body'
+    })
+  ]
 }
 
 module.exports = config
